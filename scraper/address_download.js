179585de7f1c656d4739947fb2511b7ece7e23c4
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2018-07-28 22:52:47 +0200

    [scraper] update to es6

--- scraper/address_download.js
+++ scraper/address_download.js
@@ -1,34 +1,52 @@
-const { exec } = require('child_process')
+const { exec } = require('child_process');
+const { resolve } = require('path');
 
-var minx = 369095.687897,
+const minx = 369095.687897,
   miny = 5799302.08121,
   maxx = 416868.309276,
-  maxy = 5838240.33418
+  maxy = 5838240.33418;
 
-var steps = 8, x = 0, y = 0;
+const steps = 8;
+let x = 0,
+  y = 0;
 
-function getData(){
-  console.log(x,y)
-  exec('ogr2ogr -spat '+(minx + (maxx-minx)/steps*x)+' '+(miny + (maxy-miny)/steps*y)+' '+(minx + (maxx-minx)/steps*(x+1))+' '+(miny + (maxy-miny)/steps*(y+1))+' -s_srs EPSG:25833 -t_srs WGS84 -f GeoJSON /Users/sebastianmeier/Sites/kita-explorer@tsb@github/scraper/data/address/address_'+x+'_'+y+'.geojson WFS:"http://fbinter.stadt-berlin.de/fb/wfs/geometry/senstadt/re_rbsadressen" re_rbsadressen', (err, stdout, stderr) => {
+function getData() {
+  console.log(x, y);
+  const cliParams = [
+    '-spat',
+    (minx + (maxx - minx) / steps * x),
+    (miny + (maxy - miny) / steps * y),
+    (minx + (maxx - minx) / steps * (x + 1)),
+    (miny + (maxy - miny) / steps * (y + 1)),
+    '-s_srs EPSG:25833',
+    '-t_srs WGS84',
+    '-f GeoJSON',
+    resolve(`./data/address/address_${x}_${y}.geojson`),
+    'WFS:"http://fbinter.stadt-berlin.de/fb/wfs/geometry/senstadt/re_rbsadressen"',
+    're_rbsadressen',
+  ];
+
+  exec(`ogr2ogr ${cliParams.join(' ')}`, (err, stdout, stderr) => {
     if (err) {
-      // node couldn't execute the command
-      console.log(err)
+      console.error(err);
       return;
     }
 
-    x++
-    if(x>steps){
-      x=0
-      y++
-      if(y>steps){
+    console.log(stdout, stderr);
+
+    x += 1;
+    if (x > steps) {
+      x = 0;
+      y += 1;
+      if (y > steps) {
         console.log('done');
-      }else{
-        getData()
+      } else {
+        getData();
       }
-    }else{
-      getData()
+    } else {
+      getData();
     }
-  })
+  });
 }
 
 getData();
